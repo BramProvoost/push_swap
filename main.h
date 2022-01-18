d1afd6fe8205005dc8a87adc1ed337c0a03c5887
Bram <bramprovoost@gmail.com>
2022-01-18 12:32:32 +0100

    first commit

---
+++ main.h
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.h                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: bprovoos <bprovoos@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/01/18 10:18:06 by bprovoos      #+#    #+#                 */
+/*   Updated: 2022/01/18 10:19:29 by bprovoos      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAIN_H
+# define MAIN_H
+
+int	input_valedation(int argc, char **argv);
+
+#endif
---
+++ tester.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   tester.c                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: bprovoos <bprovoos@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/01/18 10:17:29 by bprovoos      #+#    #+#                 */
+/*   Updated: 2022/01/18 10:25:28 by bprovoos      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "unity.h"
+#include "main.h"
+
+void	setUp(void) {
+	void;
+}
+
+void	tearDown(void) {
+	void;
+}
+
+void	test_input(void) {
+	void;
+}
+
+int	main(void)
+{
+	UNITY_BEGIN();
+	RUN_TEST(test_input);
+	return UNITY_END();
+}
